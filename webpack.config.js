a5698de881d001aeda82fab3756e0165510b44e1
t_daicho <daicho.t@gmail.com>
2019-05-13 02:03:37 +0900

    lintとオートフォーマットなどを追加

--- webpack.config.js
+++ webpack.config.js
@@ -4,22 +4,23 @@ module.exports = {
     mode: 'development',
     entry: './src/main.tsx',
     module: {
-        rules: [{
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-        }, {
-            test: /\.html$/,
-            loader: 'html-loader',
-        }]
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+            },
+        ],
     },
     resolve: {
-        extensions: [
-            '.ts', '.tsx', '.jsx', '.js', '.json'
-        ]
+        extensions: ['.ts', '.tsx', '.jsx', '.js', '.json'],
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: "./src/index.html"
+            template: './src/index.html',
         }),
     ],
 };
