6ac0abc84afada768453ec72137afb6a4c7b02a3
t_daicho <daicho.t@gmail.com>
2019-05-12 17:03:07 +0900

    とりあえずなんか出すまで

---
+++ src/main.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render } from 'react-dom';
+import PlayPage from './pages/play_page/play_page';
+
+render(<PlayPage />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/main.tsx',
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+        }, {
+            test: /\.html$/,
+            loader: 'html-loader',
+        }]
+    },
+    resolve: {
+        extensions: [
+            '.ts', '.tsx', '.jsx', '.js', '.json'
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        }),
+    ],
+};
